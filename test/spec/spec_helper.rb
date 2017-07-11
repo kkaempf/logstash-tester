9c781fe824091b1a71ed806c5116f7c1197b38f3
Klaus Kämpf <kkaempf@suse.de>
2017-07-11 09:55:27 +0200

    Provide GEM pathes
    and update from //github.com/logstash-plugins/logstash-patterns-core

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # Forked from: https://github.com/logstash-plugins/logstash-patterns-core
 
+Gem.paths = {
+      'GEM_HOME' => '/usr/share/logstash/vendor/bundle/jruby/1.9',
+      'GEM_PATH' => '/usr/share/logstash/vendor/bundle/jruby/1.9:/usr/share/jruby/ruby/gems/shared/'
+}
+
+$: << "/usr/share/logstash/vendor/bundle/jruby/1.9/gems/logstash-devutils-1.3.3-java/lib"
+$: << "/usr/share/logstash/logstash-core/lib"
+
 require "logstash/devutils/rspec/spec_helper"
 require 'rspec/expectations'
 
@@ -8,6 +16,19 @@ require 'rspec/expectations'
 # before requiring the grok filter
 unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
   LogStash::Environment::LOGSTASH_HOME = File.expand_path("../", __FILE__)
+end
+
+# temporary fix to have the spec pass for an urgen mass-publish requirement.
+# cut & pasted from the same tmp fix in the grok spec
+# see https://github.com/logstash-plugins/logstash-filter-grok/issues/72
+# this needs to be refactored and properly fixed
+module LogStash::Environment
+  # also :pattern_path method must exist so we define it too
+  unless self.method_defined?(:pattern_path)
+    def pattern_path(path)
+      ::File.join(LOGSTASH_HOME, "patterns", path)
+    end
+  end
 end
 
 require "logstash/filters/grok"
